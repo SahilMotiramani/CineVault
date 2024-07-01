7d3900b57cafd2bff66f6c4351e9307be768138a
Dev Gaonkar <devpgaonkar3@gmail.com>
2024-07-01 21:46:14 +0530

    add trending carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
+import TrendingMovies from './Components/TrendingMovies'
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <TrendingMovies></TrendingMovies>
     </div>
   )
 }
